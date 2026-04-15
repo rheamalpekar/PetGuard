1589dd8664ba4da81e5a7186ce04c11e31c0860e
SonicHedghog <43737412+SonicHedghog@users.noreply.github.com>
2026-04-15 03:53:27 +0000

    feat: restructure emergency handling by adding EmergencyAlertSystem and scenarios

--- PetGuardTeamProject/app/emergency/report.tsx
+++ PetGuardTeamProject/app/emergency/report.tsx
@@ -9,7 +9,7 @@ import {
   Alert,
 } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
-import { detectEmergency } from "./core/EmergencyAlertSystem";
+import { detectEmergency } from "../../src/emergency/core/EmergencyAlertSystem";
 // import { submitEmergencyReport } from "@/services/ApiResponse";
 import { addEmergencyReport } from "@/backendServices/ApiService";
 
